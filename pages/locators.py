3133151234679fcdb5a259e28ae5ba8e7751e807
SergeyRomanchikov <sergeyromanchikov@MacBook-Pro-Romanchikov.local>
2021-09-14 16:37:21 +0300

    go to login page tests refactored

--- pages/locators.py
+++ pages/locators.py
@@ -15,9 +15,13 @@ class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.XPATH, "//button[contains(@class, 'add-to-basket')]")
     CONFIRM_ALERT = (By.XPATH, "//div[@class = 'alert alert-safe alert-noicon alert-success  fade in'][1]")
     PRODUCT_NAME_IN_CONFIRM_ALERT = (
-    By.XPATH, "//div[@class = 'alert alert-safe alert-noicon alert-success  fade in'][1]//strong")
+        By.XPATH, "//div[@class = 'alert alert-safe alert-noicon alert-success  fade in'][1]//strong")
     BASKET_PRICE_IN_BASKET_ALERT = (By.XPATH, "//a[@class = 'btn btn-info']/ancestor::div/p/strong")
 
     PRODUCT_PRICE = (By.XPATH, "//p[@class = 'price_color']")
     PRODUCT_NAME = (By.XPATH, "//div[@class = 'col-sm-6 product_main']/h1")
 
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
